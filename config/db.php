78a8588b1bc840df3e150708d20c71cad427dfa0
Constantine Chuprik <constantinchuprik@gmail.com>
2016-05-01 21:07:03 +0300

    dotenv wip;

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => env('DB_DSN'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
     'charset' => 'utf8',
 ];
--- web/index.php
+++ web/index.php
@@ -1,10 +1,11 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require(__DIR__ . '/../vendor/autoload.php');
+(new \Dotenv\Dotenv(__DIR__ . '/../'))->load();
+
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG', false));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
+
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
